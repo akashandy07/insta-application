24ea5e267813234ec06c80b7ee3b7979039607e1
akashandy07 <akashandy07@gmail.com>
2026-08-07 12:43:03 -0700

    Wwaaaaaadd

--- src/pages/StoryPage.jsx
+++ src/pages/StoryPage.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { stories } from '../data/stories'
 import { users } from '../data/users'
- 
+import { useNavigate } from 'react-router-dom'
+
 
 const StoryPage = () => {
     const getUserById = (userId) => users.find(user => user.id === userId);
+    const navigate = useNavigate()
     return (
         <div className='w-[500px] mx-auto pt-5  overflow-x-auto hide-scrollbar '>
             <div className='max-w-[410px] mx-auto'>
-               
+
                 <div className='flex justify-around overflow-x-auto gap-7 hide-scrollbar'>
                     {stories.map(i => {
                         const user = getUserById(i.userId);  // ✅ Get user data
@@ -16,15 +18,17 @@ const StoryPage = () => {
                         return (
                             <div key={i.id} className='flex flex-col items-center'>
                                 {/* ✅ SHOW AVATAR IMAGE */}
-                                <div className={`w-[70px] h-[70px] rounded-full border-2 flex-shrink-0 overflow-hidden ${
-                                    i.seen 
-                                        ? 'border-gray-700' 
+                                <div className={`w-[70px] h-[70px] rounded-full border-2 flex-shrink-0 overflow-hidden ${i.seen
+                                        ? 'border-gray-700'
                                         : 'border-pink-700'
-                                }`}>
-                                    <img 
+                                    }`}>
+                                    <img
                                         src={user?.avatar}  // ✅ Avatar from user
                                         alt={user?.username}
                                         className="w-full h-full object-cover"
+                                        onClick={() =>
+                                            navigate(`/ProfilePage/${user?.id}`)
+                                        }
                                     />
                                 </div>
                                 <p className="text-xs mt-1">{user?.username}</p>
